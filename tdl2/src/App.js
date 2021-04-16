d256b898cd9190b38b877e045cc88a8e63f6d667
Ashley Oliver <ashley.p.oliver@gmail.com>
2021-04-16 09:57:26 +0100

    refactored to separate list component

--- tdl2/src/App.js
+++ tdl2/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import React from "react";
+import TodoList from "./components/TodoList"
+
+export default App;
+
+const testFixture = [
+  { id: 1, text: "Wash dishes", done: true },
+  { id: 2, text: "Do laundry", done: false },
+  { id: 3, text: "Take shower", done: false },
+];
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    { id: 1, text: "Wash dishes", done: true },
-    { id: 2, text: "Do laundry", done: false },
-    { id: 3, text: "Take shower", done: false },
-  ]);
+  const [todos, setTodos] = React.useState(testFixture);
 
   return (
     <div className="App">
@@ -14,91 +19,8 @@ function App() {
         <h1>Todo List</h1>
       </header>
       <TodoList setTodos={setTodos} todos={todos} />
-      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
 
-export default App;
 
-function TodoList({ todos, setTodos }) {
-  const handleToggle = (todo) => {
-    const updatedTodos = todos.map((t) =>
-      t.id === todo.id
-        ? {
-            ...t,
-            done: !t.done,
-          }
-        : t
-    );
-    setTodos(updatedTodos);
-  };
-  return (todos.length) ?
-    (
-    <section>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span onClick={() => handleToggle(todo)}
-              style={{
-                textDecoration: todo.done ? "line-through" : "",
-              }}
-            >
-              {todo.text}
-            </span>
-            <DeleteTodo todo={todo} setTodos={setTodos} />
-          </li>
-        ))}
-      </ul>
-    </section>
-  )
-  :
-  (<p>Empty</p>)
-}
-
-function AddTodo({ setTodos }) {
-  const inputRef = React.useRef();
-  const handleAddTodo = (e) => {
-    e.preventDefault();
-    let text = e.target.elements.addTodo.value;
-    const newdo = {
-      id: (min, max) => Math.floor(Math.random() * (max - min)) + min,
-      text,
-      done: false,
-    };
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newdo);
-    });
-    inputRef.current.value = "";
-  };
-  return (
-    <form onSubmit={handleAddTodo}>
-      <input name="addTodo" placeholder="Add todo" ref={inputRef} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-
-function DeleteTodo({ todo, setTodos }) {
-  function handleDeleteTodo() {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((t) => t.id !== todo.id);
-    });
-  }
-
-  return (
-    <div
-      onClick={handleDeleteTodo}
-      role="button"
-      style={{
-        display: "inline-block",
-        color: "red",
-        fontWeight: "bold",
-        marginLeft: 10,
-        cursor: "pointer",
-      }}
-    >
-      x
-    </div>
-  );
-}
---
+++ tdl2/src/components/TodoList.jsx
@@ -0,0 +1,87 @@
+import "../App.css";
+import React from "react";
+
+export default TodoList;
+
+function TodoList({ todos, setTodos }) {
+  const handleToggle = (todo) => {
+    const updatedTodos = todos.map((t) =>
+      t.id === todo.id
+        ? {
+            ...t,
+            done: !t.done,
+          }
+        : t
+    );
+    setTodos(updatedTodos);
+  };
+  return (todos.length) ?
+    (
+    <section className={"App-list-wrapper"}>
+      <ul className={"App-list-list"}>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span onClick={() => handleToggle(todo)}
+              style={{
+                textDecoration: todo.done ? "line-through" : "",
+              }}
+            >
+              {todo.text}
+            </span>
+            <DeleteTodo todo={todo} setTodos={setTodos} />
+          </li>
+        ))}
+      </ul>
+      <AddTodo setTodos={setTodos} />
+    </section>
+  )
+  :
+  (<p>Empty</p>)
+}
+
+function AddTodo({ setTodos }) {
+  const inputRef = React.useRef();
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    let text = e.target.elements.addTodo.value;
+    const newdo = {
+      id: (min, max) => Math.floor(Math.random() * (max - min)) + min,
+      text,
+      done: false,
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newdo);
+    });
+    inputRef.current.value = "";
+  };
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add todo" ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => t.id !== todo.id);
+    });
+  }
+
+  return (
+    <div
+      onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        display: "inline-block",
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10,
+        cursor: "pointer",
+      }}
+    >
+      x
+    </div>
+  );
+}
